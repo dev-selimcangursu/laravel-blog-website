3c11ecde1c9302628dfb4d4c71c73f0919f38810
Dev Selimcan <selimcangursu@asemsoft.com>
2025-03-14 17:25:35 +0300

    footer alanı temsili olarak tasarımı oluşturuldu.

---
+++ blog/resources/views/Client/partials/Footer.blade.php
@@ -0,0 +1,94 @@
+<footer id="footer" class="footer">
+    {{-- FooterTop:Başlangıç --}}
+    <div class="footer__top">
+        <div class="footer__top__about">
+            <img class="footer__top__about__image" src="" alt="Blog Website">
+            <p class="footer__top__about__address">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dolores?</p>
+             <br>
+            <p >Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. </p>
+            
+        </div>
+        <div class="footer__top__latest__blogs">
+            <h3>Son Yazılar</h3>
+            <div class="footer__top__latest_blog">
+                <img style="border: 1px solid whitesmoke" src="" alt="image">
+                <div class="footer__top__latest__blog__main">
+                    <span>Lorem ipsum dolor sit.</span>
+                    <div class="footer__top__latest__blog__main__bottom">
+                        <span>30 Mart 2025</span>
+                        <span><i class="bi bi-chat"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__top__latest_blog">
+                <img style="border: 1px solid whitesmoke" src="" alt="image">
+                <div class="footer__top__latest__blog__main">
+                    <span>Lorem ipsum dolor sit.</span>
+                    <div class="footer__top__latest__blog__main__bottom">
+                        <span>30 Mart 2025</span>
+                        <span><i class="bi bi-chat"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__top__latest_blog">
+                <img style="border: 1px solid whitesmoke" src="" alt="image">
+                <div class="footer__top__latest__blog__main">
+                    <span>Lorem ipsum dolor sit.</span>
+                    <div class="footer__top__latest__blog__main__bottom">
+                        <span>30 Mart 2025</span>
+                        <span><i class="bi bi-chat"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__top__latest_blog">
+                <img style="border: 1px solid whitesmoke" src="" alt="image">
+                <div class="footer__top__latest__blog__main">
+                    <span>Lorem ipsum dolor sit.</span>
+                    <div class="footer__top__latest__blog__main__bottom">
+                        <span>30 Mart 2025</span>
+                        <span><i class="bi bi-chat"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__top__categories">
+            <h3>Kategoriler</h3>
+            <div class="footer__top__categories__menu">
+                <ul>
+                    <li><a href="">Anasayfa</a></li>
+                    <li><a href="">Gelecek</a></li>
+                    <li><a href="">Tarih</a></li>
+                    <li><a href="">Kültür ve Sanat</a></li>
+                    <li><a href="">Spor</a></li>
+                    <li><a href="">Bilim ve Teknoloji</a></li>
+                    <li><a href="">Çevre</a></li>
+                    <li><a href="">Eğitim</a></li>
+                    <li><a href="">Yaşam</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">Hakkımızda</a></li>
+                    <li><a href="">İş Birliği</a></li>
+                    <li><a href="">Kariyer</a></li>
+                    <li><a href="">İletişim</a></li>
+                    <li><a href="">Giriş Yap</a></li>
+                    <li><a href="">Bağış Yap</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer__top__social">
+            <h3>Sosyal Medya</h3>
+            <ul>
+                <li><a href=""><i class="bi bi-facebook"></i></a></li>
+                <li><a href=""><i class="bi bi-instagram"></i></a></li>
+                <li><a href=""><i class="bi bi-youtube"></i></a></li>
+                <li><a href=""><i class="bi bi-twitter-x"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    {{-- FooterTop:Son --}}
+    {{-- Footer Copyright:Başlangıç --}}
+    <div class="copyright_area">
+       <small><a href="">Selimcan Gürsu</a> © 2025. Tüm Haklar Saklıdır.</small>
+    </div>
+    {{-- Footer Copyright:Son --}}
+</footer>
